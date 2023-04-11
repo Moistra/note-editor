b0021364b9dccb88298d592f3230877ee97de5f4
Artsiom Kirik <kirik.artsiom@mail.ru>
2023-04-12 00:14:43 +0300

    fix: fix bugs in displaying create note button for different devices

--- note-editor/src/components/footer/Footer.tsx
+++ note-editor/src/components/footer/Footer.tsx
@@ -10,7 +10,11 @@ interface FooterProps {
 export const Footer = ({child}: FooterProps) => {
   return (
     <footer className={style["footer-container"]}>
-      <a className={style.link} href="https://github.com/Moistra/test-tasktracker">
+      <a className={style.link}
+        href="https://github.com/Moistra/test-tasktracker"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Link to a <span className={style["link-main"]}>GitHub repo</span>
       </a>
       {child}
